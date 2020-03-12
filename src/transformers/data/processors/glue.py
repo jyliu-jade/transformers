bd83feedf213387f4bbb28c881a36697350f6a98
jyliu-jade <jl9875@nyu.edu>
2020-03-11 20:40:29 -0400

    Add BoolQProcessor and update meta dictionaries

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -515,6 +515,60 @@ class WnliProcessor(DataProcessor):
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
+class BoolQProcessor(DataProcessor):
+    """Processor for the MultiNLI data set (GLUE version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["passage"].numpy().decode("utf-8"),
+            tensor_dict["question"].numpy().decode("utf-8"),
+            str(tensor_dict["label"].numpy()),
+        )
+    """
+    def get_train_examples(self, data_dir):
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+    """
+    def get_train_examples(self, data_dir):
+        data = []
+        for line in open(os.path.join(data_dir, "train.jsonl"), 'r'):
+            dic = json.loads(line)
+            data.append([dic['passage'], dic['question'], dic['label']])
+        return self._create_examples(data, "train")
+    """
+    def get_dev_examples(self, data_dir):
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+    """
+    def get_dev_examples(self, data_dir):
+        data = []
+        for line in open(os.path.join(data_dir, "val.jsonl"), 'r'):
+            dic = json.loads(line)
+            data.append([dic['passage'], dic['question'], dic['label']])
+        return self._create_examples(data, "dev")
+    """
+    def get_test_examples(self, data_dir):
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+    """
+
+    def get_labels(self):
+        """See base class."""
+        return ["True", "False"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            try:
+                text_a = line[0]
+                text_b = line[1]
+                label = line[2]
+            except IndexError:
+                continue
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
 
 glue_tasks_num_labels = {
     "cola": 2,
@@ -526,6 +580,7 @@ glue_tasks_num_labels = {
     "qnli": 2,
     "rte": 2,
     "wnli": 2,
+    "boolq": 2,
 }
 
 glue_processors = {
@@ -539,6 +594,7 @@ glue_processors = {
     "qnli": QnliProcessor,
     "rte": RteProcessor,
     "wnli": WnliProcessor,
+    "boolq": BoolQProcessor,
 }
 
 glue_output_modes = {
@@ -552,4 +608,5 @@ glue_output_modes = {
     "qnli": "classification",
     "rte": "classification",
     "wnli": "classification",
+    "boolq": "classification",
 }
